79c073e6e9ec0641a88b5c72263e13c62035fbd9
EYATUPE <eyatuphilipesamu@gmail.com>
2024-01-02 21:49:33 +0300

    Day 4

--- Day4.py
+++ Day4.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 # Ask the user the Day today
-Day = input("What's the Day today? ")
-# Print the input
+""" Day = input("What's the Day today? ")
+Print the input
 print("Today is: " + Day)
+"""
+
+Year = input("Which year is this? ")
+print("Happy New Year,", Year)
