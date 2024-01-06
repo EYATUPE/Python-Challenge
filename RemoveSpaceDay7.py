92c1263bfd19187bbef439fbf7e2888f545ed4c0
EYATUPE <eyatuphilipesamu@gmail.com>
2024-01-06 20:24:00 +0300

    Day 7

--- RemoveSpaceDay7.py
+++ RemoveSpaceDay7.py
@@ -1,3 +1,9 @@
 #!/usr/bin/env python3
+#Ask user for their Name
 Name = input("What's your Name? ")
+
+#Remove Whitespaces from the str
+Name = Name.strip()
+
+#Print User's Name
 print = (f"Hello, {Name}")
