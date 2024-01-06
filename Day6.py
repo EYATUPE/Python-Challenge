9e75acae0ff8fcfd6b53184d17700f9ec984ceab
EYATUPE <eyatuphilipesamu@gmail.com>
2024-01-06 20:39:18 +0300

    Day 6

--- Day6.py
+++ Day6.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-PHILIP = input("Which Day of Python is? ")
+PHILIP = input("Which Day of Python is? ").strip().title()
 print("Well done, " + PHILIP)
 GREET = input("How are you? " + PHILIP)
 print("PHILIP " + GREET)
