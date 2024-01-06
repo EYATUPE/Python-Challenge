39f0b9c9166174ce51f25cafeafdb8412882611a
EYATUPE <eyatuphilipesamu@gmail.com>
2024-01-06 20:51:04 +0300

    Day 7

---
+++ SplitnameDay7.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# Ask User their Name
+name = input("What's Your Name? ").strip().title()
+
+# Split User's Name
+firt, Last = name.split()
+
+#Print User's Name
+print("Hello, " {first})
