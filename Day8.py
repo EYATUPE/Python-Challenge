3fdcfdc2149db1eb43c9415d5138ae555c02d585
EYATUPE <eyatuphilipesamu@gmail.com>
2024-01-09 00:24:28 +0300

    Day 8 Calculator

--- Day8.py
+++ Day8.py
@@ -13,3 +13,14 @@ x = input("What's x ? ")
 y = input("What's y ? ")
 z = x + y
 print(z)
+
+"""The Second Example will just concatenate the above 
+output instead of adding it up but look in example
+3 for the better options
+"""
+
+#Example 3
+x = input("What's x ? ")
+y = input("What's y ? ")
+z = int(x) + int(y)
+print(z)
