caa57e3107fa85d3a01f78b1020aa59d92457509
EYATUPE <eyatuphilipesamu@gmail.com>
2024-01-11 12:29:09 +0300

    Day 10

--- Day10.py
+++ Day10.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
-x = float(input=("What's X ?")
-y = float(input=("What's Y ?")
-print(x / y)
+x = float(input("What's X ?"))
+y = float(input("What's Y ?"))
+z = (x / y)
+print(f"{z:,2}")
